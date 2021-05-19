7c0d143e808979b2dd8cb5b7f17fef5936e54860
Gustavo Viegas <gustavoviegas231914@gmail.com>
2021-05-19 16:49:55 -0300

    :bug: Ultimo Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const routes = require('./routes');
 const app = express();
-const port = process.env.PORT !! 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 
